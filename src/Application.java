fcb8932c6dfb8b4e28b56881ffc3a8da5250197c
jcollinsstudent <joshuaolincollins@gmail.com>
2021-08-21 15:03:55 -0400

    Updated

--- src/Application.java
+++ src/Application.java
@@ -1,16 +1,22 @@
 
 public class Application {
 
+	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public static void main(String[] args) {
 		Item<String> stringItem = new Item<String>();
 		stringItem.setE("Josh Collins");
 		Item<Integer> intItem = new Item<Integer>();
 		intItem.setE(22);
 		
-		SmallBag<Item> sb = new SmallBag<Item>();
-		sb.setItem(intItem);
-		Item<Integer> returnedItem = sb.getItem();
-		System.out.println(returnedItem.getE());
+		SmallBag<Item> stringSB = new SmallBag<Item>();
+		stringSB.setItem(stringItem);
+		Item<String> returnedStringItem = stringSB.getItem();
+		System.out.println(returnedStringItem.getE());
+		
+		SmallBag<Item> intSB = new SmallBag<Item>();
+		intSB.setItem(intItem);
+		Item<Integer> returnedIntItem = intSB.getItem();
+		System.out.println(returnedIntItem.getE());
 		
 	}
 
